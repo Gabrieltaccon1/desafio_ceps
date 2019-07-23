0e71fb2a5cdebc32e41117dcb23d7432fc0c5f8c
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-07-23 11:16:03 -0300

    correcao de erros

--- Entities/Endereco.cs
+++ Entities/Endereco.cs
@@ -15,6 +15,7 @@ namespace ConsoleApp36.Entities
         public string Unidade { get; set; }
         public string Ibge { get; set; }
         public string Gia { get; set; }
+        public string datadeConsulta { get; set; }
 
 
         public Endereco()
@@ -22,7 +23,7 @@ namespace ConsoleApp36.Entities
 
         }
 
-        public Endereco(string cep, string logradouro, string complemento, string bairro, string localidade,string uf, string unidade, string ibge, string gia)
+        public Endereco(string cep, string logradouro, string complemento, string bairro, string localidade, string uf, string unidade, string ibge, string gia)
         {
             Cep = cep;
             Logradouro = logradouro;
@@ -36,6 +37,75 @@ namespace ConsoleApp36.Entities
 
         }
 
+        public void PopularaBase(List<string> endereco)
+        {
+
+
+
+            {
+                endereco.Add("03707030");
+                endereco.Add("08340030");
+                endereco.Add("08373750");
+                endereco.Add("08431030");
+                endereco.Add("05782430");
+
+                endereco.Add("08452480");
+                endereco.Add("04939220");
+                endereco.Add("01417030");
+                endereco.Add("04717903");
+                endereco.Add("05458050");
+
+                endereco.Add("08021270");
+                endereco.Add("02220365");
+                endereco.Add("03312000");
+                endereco.Add("03807000");
+                endereco.Add("04709130");
+
+                endereco.Add("08090310");
+                endereco.Add("01540070");
+                endereco.Add("04544140");
+                endereco.Add("02807120");
+                endereco.Add("02817050");
+
+                endereco.Add("01415010");
+                endereco.Add("01529000");
+                endereco.Add("02710000");
+                endereco.Add("02956020");
+                endereco.Add("02262060");
+
+                endereco.Add("04867010");
+                endereco.Add("03736005");
+                endereco.Add("03424010");
+                endereco.Add("03310010");
+                endereco.Add("02175040");
+
+                endereco.Add("04006030");
+                endereco.Add("02237044");
+                endereco.Add("05893040");
+                endereco.Add("03254250");
+                endereco.Add("08473627");
+
+                endereco.Add("05131040");
+                endereco.Add("08121473");
+                endereco.Add("02723007");
+                endereco.Add("04646030");
+                endereco.Add("01415009");
+
+                endereco.Add("03245090");
+                endereco.Add("08071220");
+                endereco.Add("05281020");
+                endereco.Add("02218040");
+                endereco.Add("03515110");
+
+                endereco.Add("04429190");
+                endereco.Add("01444060");
+                endereco.Add("01004010");
+                endereco.Add("03516040");
+                endereco.Add("02231030");
+
+            }
+
+        }
     }
 }
                         
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using ConsoleApp36.Entities;
+using System.Linq;
 
 
 namespace ConsoleApp36
@@ -12,127 +13,96 @@ namespace ConsoleApp36
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("QUANTOS REGISTROS POR PAGINA? ");
-            // int decisao = int.Parse(Console.ReadLine());
+            Endereco e = new Endereco();
+            List<string> endereco = new List<string>();
+            List<Endereco> Zipcode = new List<Endereco>();
+            e.PopularaBase(endereco);
+            int contagem = 0;
+            int decisao;
+            string decisaotwo;
 
 
-            List<Endereco> buscaceps = new List<Endereco>()
+            foreach (var zipcode in endereco)
             {
-                new Endereco(){Cep = "03707030" },
-                new Endereco(){Cep = "08340030"},
-                new Endereco(){Cep = "08373750" },
-                new Endereco(){Cep = "08431030" },
-                new Endereco(){Cep = "05782430" },
-
-                new Endereco(){Cep = "08452480"},
-                new Endereco(){Cep = "04939220"},
-                new Endereco(){Cep = "01417030" },
-                new Endereco(){Cep = "04717903" },
-                new Endereco(){Cep = "05458050"},
-
-                new Endereco(){Cep = "08021270"},
-                new Endereco(){Cep = "02220365"},
-                new Endereco(){Cep = "03312000"},
-                new Endereco(){Cep = "03807000"},
-                new Endereco(){Cep = "04709130"},
-
-                new Endereco(){Cep = "08090310" },
-                new Endereco(){Cep = "01540070" },
-                new Endereco(){Cep = "04544140" },
-                new Endereco(){Cep = "02807120" },
-                new Endereco(){Cep = "02817050" },
-
-                new Endereco(){Cep = "01415010" },
-                new Endereco(){Cep = "01529000" },
-                new Endereco(){Cep = "02710000" },
-                new Endereco(){Cep = "02956020" },
-                new Endereco(){Cep = "02262060" },
-
-                new Endereco(){Cep = "04867010" },
-                new Endereco(){Cep = "03736005" },
-                new Endereco(){Cep = "03424010" },
-                new Endereco(){Cep = "03310010" },
-                new Endereco(){Cep = "02175040" },
-
-                new Endereco(){Cep = "04006030" },
-                new Endereco(){Cep = "02237044" },
-                new Endereco(){Cep = "05893040" },
-                new Endereco(){Cep = "03254250" },
-                new Endereco(){Cep = "08473627" },
-
-                new Endereco(){Cep = "05131040"},
-                new Endereco(){Cep = "08121473"},
-                new Endereco(){Cep = "02723007"},
-                new Endereco(){Cep = "04646030"},
-                new Endereco(){Cep = "01415009"},
-
-                new Endereco(){Cep = "03245090"},
-                new Endereco(){Cep = "08071220"},
-                new Endereco(){Cep = "05281020"},
-                new Endereco(){Cep = "02218040"},
-                new Endereco(){Cep = "03515110"},
-
-                new Endereco(){Cep = "04429190"},
-                new Endereco(){Cep = "01444060"},
-                new Endereco(){Cep = "01004010"},
-                new Endereco(){Cep = "03516040"},
-                new Endereco(){Cep = "02231030"}
-            };
-
-            foreach (Endereco findcep in buscaceps)
-            {
-                
-                string https = string.Empty;
-                string url = $"https://viacep.com.br/ws/{findcep.Cep}/json";
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                request.AutomaticDecompression = DecompressionMethods.GZip;
-
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
+                try
                 {
-                    https = reader.ReadToEnd();
+
+                    var requisicaoWeb = WebRequest.CreateHttp($"https://viacep.com.br/ws/{Zipcode}/json");
+                    requisicaoWeb.Method = "GET";
+                    requisicaoWeb.UserAgent = "RequisicaoWebDemo";
+
+                    using (var resposta = requisicaoWeb.GetResponse())
+                    {
+                        var streamDados = resposta.GetResponseStream();
+                        StreamReader reader = new StreamReader(streamDados);
+                        object objResponse = reader.ReadToEnd();
+                        var local = JsonConvert.DeserializeObject<Endereco>(objResponse.ToString());
+                        local.datadeConsulta = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss:ff");
+                        Zipcode.Add(local);
+                        contagem++;
+                    }
+                    Zipcode = Zipcode.OrderBy(x => x.Uf).ThenBy(x => x.datadeConsulta).ToList();
+                    string path = @"C:\Users\Treinamento 4\Documents\consultaCep/consultaceps.txt";
+                    // convertendo e escrevendo o json 
+                    StreamWriter sw2 = new StreamWriter(path);
+                    string g2 = JsonConvert.SerializeObject(Zipcode);
+                    sw2.WriteLine(g2);
+                    sw2.Close();
                 }
-
-                Console.WriteLine(https);
-
-
-
-                StreamWriter sw2 = new StreamWriter(@"C:\Users\Treinamento 4\Desktop\Aula05");
-                Endereco g2 = JsonConvert.DeserializeObject<Endereco>(https);
-                buscaceps.Add(findcep);
-                sw2.WriteLine(g2);
-
-                sw2.Close();
-
+                catch (WebException f)
+                {
+                    Console.WriteLine(f.Message);
+                }
+                Console.WriteLine($"FORAM REGISTRADOS UM TOTAL DE: {contagem} CEP'S");
             }
 
 
 
 
+            Console.WriteLine("QUANTOS REGISTROS POR PAGINA VOCÊ DESEJA CONSULTAR? ");
+            int registrosDesejados = int.Parse(Console.ReadLine());
+            int paginacao = 50 / registrosDesejados;
+
+            if (50 % registrosDesejados !=0)
+            {
+                paginacao = paginacao + 1;
+            }
 
 
 
-            //string cep;
+            do
+            {
 
-            //Console.WriteLine("Informe o Cep Desejado");
-            //cep = Console.ReadLine();
+                for (int i = 1; i < paginacao; i++)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"DIGITE {i} PARA VISUALIZAR A PAGINA {i}");
 
-            // while (cep < 10000000 || cep > 99999999) 
-            //  {
-            // Console.WriteLine("Informe o Cep Desejado");
-            // cep = Console.ReadLine();
-            //  }
+                }
 
+                Int32.TryParse(Console.ReadLine(), out decisao);
 
+                if (decisao <= 0 || decisao > paginacao)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("!!! A PAGINA DIGITADA NÃO EXISTE NO CONTEXTO ATUAL !!! ");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    var result = Zipcode.Skip((decisao - 1) * registrosDesejados).Take(registrosDesejados);
 
+                    foreach (var zip in result)
+                    {
+                        Console.WriteLine($"CEP: {zip.Cep} | LOGRADOURO: {zip.Logradouro} | BAIRRO: {zip.Bairro} | UF: {zip.Uf} | HORARIO DE CONSULTA DO CEP: {zip.datadeConsulta}");
+                    }
+                }
 
+                Console.WriteLine("DESEJA VISUALIZAR OUTRA PAGINA/CONTINUAR NO PROGRAMA? ");
+                Console.WriteLine("             DIGITE ( SIM ) OU ( NÃO )                ");
+                decisaotwo = Console.ReadLine().ToUpper();
 
-
-
-
-
-
+            } while (decisaotwo == "SIM");
 
 
         }
